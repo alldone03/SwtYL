d7ccb0992342291514e404330e6ed2d336611510
alldone03 <aldanarsenal@gmail.com>
2024-02-21 14:07:06 +0700

    FEAT: Show Image To pyQT

---
+++ code/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+def showToUI(self,frame):
+    #     pixmap = QPixmap.fromImage(frame)
+    #     pixmap = pixmap.scaled(self.Camera.size(), Qt.KeepAspectRatio)
+    #     self.Camera.setPixmap(pixmap)
